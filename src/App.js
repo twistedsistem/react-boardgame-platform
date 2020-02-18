8e06aebc404f2d8b9b11800e7385505ee2c4e983
twistedsistem <twistedsistem@gmail.com>
2020-02-18 17:08:06 -0500

    main game view and player decks tabbed area

--- src/App.js
+++ src/App.js
@@ -1,147 +1,64 @@
-import React, { useState } from "react";
+import React from "react";
+import "./App.scss"
 import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
-import uuid from "uuid/v4";
 
-const itemsFromBackend = [
-  { id: uuid(), content: "First task" },
-  { id: uuid(), content: "Second task" },
-  { id: uuid(), content: "Third task" },
-  { id: uuid(), content: "Fourth task" },
-  { id: uuid(), content: "Fifth task" }
-];
-
-const columnsFromBackend = {
-  [uuid()]: {
-    name: "Requested",
-    items: itemsFromBackend
-  },
-  [uuid()]: {
-    name: "To do",
-    items: []
-  },
-  [uuid()]: {
-    name: "In Progress",
-    items: []
-  },
-  [uuid()]: {
-    name: "Done",
-    items: []
+class Tabs extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selected:this.props.selected || 0
+    };
   }
-};
-
-const onDragEnd = (result, columns, setColumns) => {
-  if (!result.destination) return;
-  const { source, destination } = result;
-
-  if (source.droppableId !== destination.droppableId) {
-    const sourceColumn = columns[source.droppableId];
-    const destColumn = columns[destination.droppableId];
-    const sourceItems = [...sourceColumn.items];
-    const destItems = [...destColumn.items];
-    const [removed] = sourceItems.splice(source.index, 1);
-    destItems.splice(destination.index, 0, removed);
-    setColumns({
-      ...columns,
-      [source.droppableId]: {
-        ...sourceColumn,
-        items: sourceItems
-      },
-      [destination.droppableId]: {
-        ...destColumn,
-        items: destItems
-      }
-    });
-  } else {
-    const column = columns[source.droppableId];
-    const copiedItems = [...column.items];
-    const [removed] = copiedItems.splice(source.index, 1);
-    copiedItems.splice(destination.index, 0, removed);
-    setColumns({
-      ...columns,
-      [source.droppableId]: {
-        ...column,
-        items: copiedItems
-      }
-    });
+  render(){
+    return (<div>
+      <ul className="inline">
+        {this.props.children.map((elem,index)=>{
+          let style = index === this.state.selected ? 'selected': '';
+         return <li className={style} key={index} onClick={this.handleChange.bind(this,index)}>{elem.props.title}</li>
+        })}
+      </ul>
+      <div className="tab">{this.props.children[this.state.selected]}</div>
+      </div>
+    )
   }
-};
+  handleChange(index){
+    this.setState({selected:index})
+  }
+}
+
+class Panel extends React.Component {
+  render(){
+    return <div>{this.props.children}</div>
+  }
+}
 
 function App() {
-  const [columns, setColumns] = useState(columnsFromBackend);
   return (
-    <div style={{ display: "flex", justifyContent: "center", height: "100%" }}>
-      <DragDropContext
-        onDragEnd={result => onDragEnd(result, columns, setColumns)}
+    <div style={{ display: "flex", flexDirection: "column", height: "100%" }}>
+      <div
+        style={{
+          display: "flex",
+          border: "1px solid black",
+          height: "75%",
+          justifyContent: "center",
+          alignItems: "center"
+        }}
       >
-        {Object.entries(columns).map(([columnId, column], index) => {
-          return (
-            <div
-              style={{
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center"
-              }}
-              key={columnId}
-            >
-              <h2>{column.name}</h2>
-              <div style={{ margin: 8 }}>
-                <Droppable droppableId={columnId} key={columnId}>
-                  {(provided, snapshot) => {
-                    return (
-                      <div
-                        {...provided.droppableProps}
-                        ref={provided.innerRef}
-                        style={{
-                          background: snapshot.isDraggingOver
-                            ? "lightblue"
-                            : "lightgrey",
-                          padding: 4,
-                          width: 250,
-                          minHeight: 500
-                        }}
-                      >
-                        {column.items.map((item, index) => {
-                          return (
-                            <Draggable
-                              key={item.id}
-                              draggableId={item.id}
-                              index={index}
-                            >
-                              {(provided, snapshot) => {
-                                return (
-                                  <div
-                                    ref={provided.innerRef}
-                                    {...provided.draggableProps}
-                                    {...provided.dragHandleProps}
-                                    style={{
-                                      userSelect: "none",
-                                      padding: 16,
-                                      margin: "0 0 8px 0",
-                                      minHeight: "50px",
-                                      backgroundColor: snapshot.isDragging
-                                        ? "#263B4A"
-                                        : "#456C86",
-                                      color: "white",
-                                      ...provided.draggableProps.style
-                                    }}
-                                  >
-                                    {item.content}
-                                  </div>
-                                );
-                              }}
-                            </Draggable>
-                          );
-                        })}
-                        {provided.placeholder}
-                      </div>
-                    );
-                  }}
-                </Droppable>
-              </div>
-            </div>
-          );
-        })}
-      </DragDropContext>
+        <h2>Main Game Board Area</h2>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          border: "5px solid orange",
+          height: "25%",
+        }}
+      >
+        <Tabs selected={1}>
+          <Panel title="First Player">This is the first panel</Panel>
+          <Panel title="Second Player">This is the second panel</Panel>
+          <Panel title="Shared Decks">This is the third panel</Panel>
+        </Tabs>
+      </div>
     </div>
   );
 }
