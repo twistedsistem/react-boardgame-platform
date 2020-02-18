544c8a8e07a98bab9f985b8cf4c96e44a42f70c5
twistedsistem <twistedsistem@gmail.com>
2020-02-17 22:05:26 -0500

    started work on player area tabs in konva

--- src/Card.js
+++ src/Card.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Layer, Rect} from 'react-konva';
+import {Rect} from 'react-konva';
 
 class Card extends Component {
     state = {
@@ -10,29 +10,27 @@ class Card extends Component {
 
     render() {
         return (
-            <Layer>
-                <Rect
-                    x={this.state.x}
-                    y={this.state.y}
-                    draggable
-                    width={100}
-                    height={100}
-                    fill="red"
-                    shadowBlur={10}
-                    onDragStart={() => {
-                        this.setState({
-                        isDragging: true
-                        });
-                    }}
-                    onDragEnd={e => {
-                        this.setState({
-                        isDragging: false,
-                        x: e.target.x(),
-                        y: e.target.y()
-                        });
-                    }}
-                />
-            </Layer>
+            <Rect
+                x={this.state.x}
+                y={this.state.y}
+                draggable
+                width={100}
+                height={100}
+                fill="red"
+                shadowBlur={10}
+                onDragStart={() => {
+                    this.setState({
+                    isDragging: true
+                    });
+                }}
+                onDragEnd={e => {
+                    this.setState({
+                    isDragging: false,
+                    x: e.target.x(),
+                    y: e.target.y()
+                    });
+                }}
+            />
         );
     }
 }
